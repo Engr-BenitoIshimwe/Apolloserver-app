57c6f7b284d5f4057e850c80274f8d7faf804dfe
BenitoIshimwe <benishimwe31@gmail.com>
2022-12-14 11:53:02 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const { buildSchema, graphql } = require('graphql');
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+
+const app = express();
+
+const schema = buildSchema(`
+ type Query{
+  name: String,
+  email: String
+ } 
+`);
+
+const root = {
+  name: () => {
+    return 'Sarthak';
+  },
+  email: () => {
+    return 'sarthak@bitfumes.com';
+  },
+};
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(8000);
+
+console.log('Server is running at http://localhost:8000');
